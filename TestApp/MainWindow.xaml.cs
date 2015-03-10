7ea91824d6a89125da9dd33114420b655ea841d4
Spencer Briere <spen1994cer@hotmail.com>
2015-03-09 20:36:39 -0600

    Forced Commit

--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -24,5 +24,36 @@ namespace TestApp
         {
             InitializeComponent();
         }
+        
+        private void EnterDown(object sender, KeyEventArgs e)
+        {
+
+            TabItem tempTab = (TabItem)playerTabCont.Items.CurrentItem;
+            Grid tempGrid = (Grid)playerTabCont.SelectedContent;
+            UIElementCollection tempUI = tempGrid.Children;
+            TextBox tempText = (TextBox)tempUI[0];
+
+
+            if (e.Key == Key.Enter) 
+            {
+                
+
+                tempText.AppendText("\nYou: " + DMChat.Text);
+                DMChat.Text = "";
+                int tmp = tempText.LineCount;
+                tempText.ScrollToLine(tmp-1);
+            }
+        }
+
+        private void addTab(object sender, MouseButtonEventArgs e)
+        {
+            int thisTab = playerTabCont.SelectedIndex;
+            
+
+            TabItem newTab = new TabItem { DataContext = defaultTab.DataContext };
+            newTab.Header = "Stuff";
+            newTab.Visibility = Visibility.Visible;
+            playerTabCont.Items.Add(newTab);
+        }
     }
 }
